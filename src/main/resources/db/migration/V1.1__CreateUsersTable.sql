e2b210ebea336572b5e4caf835a8fa254f721765
Narayana Vijaya Durga <durga@Narayanas-MacBook-Pro.local>
2022-06-12 23:10:20 +0530

    talismanrc file added

---
+++ src/main/resources/db/migration/V1.1__CreateUsersTable.sql
@@ -0,0 +1,5 @@
+create table users (
+    id int not null,
+    name varchar(255) not null,
+    address varchar(255) not null
+);
---
+++ src/main/resources/db/migration/V1.2__CreateProductsTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE products(
+ id int not null,
+ name VARCHAR(255) not null,
+ price VARCHAR(255) not null
+);
